0ff0e553aff81ed7c325854a3a7af9dd763a341a
SouzaDevv <giovannidesouzasantos20092007@gmail.com>
2026-08-09 12:37:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from interface import iniciar_app
 
 if __name__ == "__main__":
 <<<<<<< HEAD
-    criar_tabela()   #
+    criar_tabela()   
 =======
     criar_tabela()  
 >>>>>>> a07d9a29f0f781f9493ffb69a6ed902ba447f804
